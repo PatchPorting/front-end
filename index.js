1e9bb408ca2c641478fca52b64f6f61bd47f3d08
Joseph Schwarz <josephschwarz@Josephs-MBP.lan>
2017-08-17 14:43:10 -0400

    Update user script to serve and load custom script from server side

--- index.js
+++ index.js
@@ -19,7 +19,17 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/greasemonkey', function(req, res) {
+	res.sendFile(__dirname + '/script.js');
+});
 
 app.listen(port, function() {
 	console.log('Listening on port: ' + port + '...');
